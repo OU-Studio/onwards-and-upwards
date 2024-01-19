973ef8b2ed7312a3a18ce6d9a14586e915e37e05
Joe Cory <joe@chair8media.com>
2024-01-19 11:05:18 +0000

    a

--- app/page.tsx
+++ app/page.tsx
@@ -5,26 +5,7 @@ function HomePage() {
     <div>
       {/* Header with Navigation Links */}
       <header>
-        <nav>
-          <ul>
-            <li>
-              <Link href="/">
-Home              </Link>
-            </li>
-            <li>
-              <Link href="/about">
-About              </Link>
-            </li>
-            <li>
-              <Link href="/blog">
-Blog              </Link>
-            </li>
-            <li>
-              <Link href="/contact">
-Contact              </Link>
-            </li>
-          </ul>
-        </nav>
+        <a href="/" className="logo"></a>
       </header>
 
       {/* Main Content */}
